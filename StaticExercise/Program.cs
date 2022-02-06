3295a5c3c5d4138334cdc3d3371c4f4c88e6a3b2
Joe Zedd <zychjoe@gmail.com>
2022-02-06 11:09:08 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Boiling: Celsius = {TempConverter.FahrenheitToCelsius(212)}, Fahrenheit: {TempConverter.CelsiusToFahrenheit(100)}");
+            Console.WriteLine($"Freezing: Celsius = {TempConverter.FahrenheitToCelsius(32)}, Fahrenheit: {TempConverter.CelsiusToFahrenheit(0)}");
+            Console.WriteLine($"122F / 50C : Celsius = {TempConverter.FahrenheitToCelsius(122)}, Fahrenheit: {TempConverter.CelsiusToFahrenheit(50)}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double fahr)
+        {
+            return (fahr - 32) * (5.0 / 9.0);
+
+        }
+
+        public static double CelsiusToFahrenheit(double cels)
+        {
+            return (cels * (9.0/5.0)) + 32;
+        }
+    }
+}
